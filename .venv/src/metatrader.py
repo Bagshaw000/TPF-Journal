b7144eed74a5c63e1a94f4ba2482138be0c53e61
Omieibi Bagshaw <omieibibagshaw007@gmail.com>
2025-07-30 13:03:03 +1200

    Update metatrader.py

--- .venv/src/metatrader.py
+++ .venv/src/metatrader.py
@@ -1,14 +1,14 @@
-import MetaTrader5 as mt5
+# import MetaTrader5 as mt5
 
-class MetaTrader:
-    def __init__(self ):
-        if not mt5.initialize():
-            print("Initiliazed failed, error code",mt5.last_error())
-        print(mt5.terminal_info())
+# class MetaTrader:
+#     def __init__(self ):
+#         if not mt5.initialize():
+#             print("Initiliazed failed, error code",mt5.last_error())
+#         print(mt5.terminal_info())
         
-    def login(login:int, server:str, password:str):
-        try:
-            mt5.login(login, server, password)
-            mt5.shutdown()
-        except:
-            print("Error login")
+#     def login(login:int, server:str, password:str):
+#         try:
+#             mt5.login(login, server, password)
+#             mt5.shutdown()
+#         except:
+#             print("Error login")
